d9a5478136e5ae867085457f2ff3a063b4ee5e41
Miyuru Dharmage <voxsar@gmail.com>
2022-11-29 23:56:50 +0530

    nc hook api

--- artslab-nc-hook-api.php
+++ artslab-nc-hook-api.php
@@ -49,4 +49,4 @@ function add_new_user_save_action( $form_actions_registrar ) {
 add_action( 'elementor_pro/forms/actions/register', 'add_new_user_save_action' );
 
 require_once( __DIR__ . '/updater.php' );
-new ALCES2Updater();
+new ALCNCHUpdater();
